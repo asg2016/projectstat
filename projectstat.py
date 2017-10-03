a1353f5fc994b5037b7f54833c9dda11f3f6023f
Alex Gladkov <asg0182@gmail.com>
2017-10-03 21:46:05 +0500

    to_json to_csv

--- projectstat.py
+++ projectstat.py
@@ -1,10 +1,22 @@
 import argparse
+from projectstat.projects import PythonProject
+from projectstat.reports import ProjectReport, ModuleReport
 
 
 def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('path', nargs='p')
-    parser.add_argument('path', nargs='p')
+    parser = argparse.ArgumentParser(description='Build some stat about your projects.')
+    parser.add_argument('path', action='store', type=str,
+                        help='path to local project')
+    parser.add_argument('git', action='store', type=str,
+                        help='clone url for project.git')
+    parser.add_argument('--common', action='store_true',
+                        help='common project stat')
+    parser.add_argument('--detail', action='store_true',
+                        help='detail module stat')
+    parser.add_argument('-topsize', action='store',
+                        help='top size for project')
+    parser.add_argument('-maxfiles', action='store',
+                        help='max files for processing in project')
     return parser
 
 
--- projectstat/reports.py
+++ projectstat/reports.py
@@ -1,18 +1,36 @@
+import json
+import csv
 from collections import Counter
 from .stat import get_frequency_word_stat
 
 
-class ModuleStat():
+class BaseReport():
+    def __init__(self):
+        self.stat = {}
+
+
+    def to_json(self, filename):
+        with open(filename, 'wb') as f:
+            f.writelines(json.dumps(self.stat))
+
+
+    def to_csv(self, filename):
+        with open(filename, 'wb') as f:
+            w = csv.writer(f)
+            w.writerows(self.stat.items())
+
+
+class ModuleReport(BaseReport):
     def __init__(self, module, top_size):
-        self.module_stat = {'def': {}, 'var': {}, 'class': {}}
+        self.stat = {'def': {}, 'var': {}, 'class': {}}
         self.__build_stat__(module, top_size)
 
     def __build_stat__(self, module, top_size):
         for items_key, items_val in module.names.items():
-            self.module_stat[items_key] = get_frequency_word_stat(items_val, top_size)
+            self.stat[items_key] = get_frequency_word_stat(items_val, top_size)
 
 
-class ProjectReport():
+class ProjectReport(BaseReport):
     def __init__(self, project, top_size):
         self.top_size = top_size
         self.stat = self.__build_stat__(project, top_size)
